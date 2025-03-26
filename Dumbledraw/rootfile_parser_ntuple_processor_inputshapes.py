4fcb138ae94e0cddcfab6543a578c3ce6102ce40
Artur Monsch <60860160+a-monsch@users.noreply.github.com>
2025-03-26 13:49:48 +0100

    adjusting colors for jetfakes

--- Dumbledraw/rootfile_parser_ntuple_processor_inputshapes.py
+++ Dumbledraw/rootfile_parser_ntuple_processor_inputshapes.py
@@ -37,7 +37,6 @@ class Rootfile_parser(object):
         "jetFakes_NLO": "jetFakesMC_NLO",
         "ggH125": "ggH",
         "qqH125": "qqH",
-        "wFakes": "wFakes",
     }
 
     _process_map = {
@@ -65,7 +64,6 @@ class Rootfile_parser(object):
         "jetFakes": "jetFakesMC",
         "ggH125": "ggH125",
         "qqH125": "qqH125",
-        "wFakes": "wFakes",
     }
 
     def __init__(self, inputrootfilename, variable):
