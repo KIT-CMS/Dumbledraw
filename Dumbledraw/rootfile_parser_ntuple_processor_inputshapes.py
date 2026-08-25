4e990a92e3f4469ed981c99cc94f97a78755dc15
jvoss <jan.voss2@kit.edu>
2026-08-25 16:30:03 +0200

    Last used changes for v15 DT2p5 SFs

--- Dumbledraw/rootfile_parser_ntuple_processor_inputshapes.py
+++ Dumbledraw/rootfile_parser_ntuple_processor_inputshapes.py
@@ -77,26 +77,34 @@ class Rootfile_parser(object):
 
     
 
-    def __init__(self, inputrootfilename, variable, es_range):
+    def __init__(self, inputrootfilename, variable, es_range, tes_precision=None, channel=None):
         self._rootfilename = inputrootfilename
         self._rootfile = ROOT.TFile(self._rootfilename, "READ")
         self._variable = variable
         self.es_range = es_range
+        self.tes_precision = tes_precision
+        self._channel = channel
         if None not in es_range:
             emb_map_proc = {"EMB": "Embedded",
             **{f"emb{'minus' if val < 0 else ''}{str(round(abs(val), 1)).replace('.', 'p')}": 
-            "Embedded" for val in np.arange(self.es_range[1], self.es_range[0] - 0.1, -0.1) if val != 0.0
+            "Embedded" for val in np.arange(self.es_range[1], self.es_range[0] - self.tes_precision, -self.tes_precision) if val != 0.0
             } }
             emb_map = { "EMB": "EMB",
             **{f"emb{'minus' if val < 0 else ''}{str(round(abs(val), 1)).replace('.', 'p')}":
             f"emb{'minus' if val < 0 else ''}{str(round(abs(val), 1)).replace('.', 'p')}"
-            for val in np.arange(self.es_range[1], self.es_range[0] - 0.1, -0.1) if val != 0.0
+            for val in np.arange(self.es_range[1], self.es_range[0] - self.tes_precision, -self.tes_precision) if val != 0.0
             }}
             self._dataset_map = {**_dataset_map_no_emb, **emb_map}
             self._process_map = {**_process_map_no_emb, **emb_map_proc}
         else:
-            self._dataset_map = copy.deepcopy(_dataset_map_no_emb)
-            self._process_map = copy.deepcopy(_process_map_no_emb)
+            if self._channel == "mm" or self.tes_precision == None:
+                emb_map_proc = {"EMB": "Embedded"}
+                emb_map = {"EMB": "EMB"}
+                self._dataset_map = {**_dataset_map_no_emb, **emb_map}
+                self._process_map = {**_process_map_no_emb, **emb_map_proc}
+            else:
+                self._dataset_map = copy.deepcopy(_dataset_map_no_emb)
+                self._process_map = copy.deepcopy(_process_map_no_emb)
     @property
     def rootfile(self):
         return self._rootfile
